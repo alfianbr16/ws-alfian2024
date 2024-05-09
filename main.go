ddcb2b4cd6fec351244d96f8d425590ab15acb48
alfianbr16 <alfianbr84@gmail.com>
2024-05-09 22:57:48 +0700

    update

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"log"
 
-	"iteung/config"
+	"github.com/alfianbr16/ws-2024/config"
 
 	"github.com/aiteung/musik"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 
 
-	"iteung/url"
+	"github.com/alfianbr16/ws-2024/url"
 
 	"github.com/gofiber/fiber/v2"
 )
--- url/url.go
+++ url/url.go
@@ -1,7 +1,7 @@
 package url
 
 import (
-	"iteung/controller"
+	"github.com/alfianbr16/ws-2024/controller"
 
 	"github.com/gofiber/fiber/v2"
 )
